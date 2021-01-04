9fbd9aa4bdbdb6e8432b12bdf1dbe3f7f627f073
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-04 12:06:10 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,5 +1,5 @@
 'use strict'
-function play(messageEvent){
+function tock(messageEvent){
 	let length = (messageEvent.data.length/2) - 2;
 	let lengthFull = messageEvent.data.length - 1;	// Do not count opposite store.
 	// Go from right.
@@ -20,6 +20,6 @@ function play(messageEvent){
 	}
 }
 onmessage = messageEvent => {
-	onmessage = play;
+	onmessage = tock;
 	postMessage(null);
 };
