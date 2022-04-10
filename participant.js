5d51a9a44ad09f17f28eed9bffe94e0919547217
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-04-10 10:37:29 +0200

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,22 +1,24 @@
 'use strict'
-ParticipantHelper.onmessage = message => {
-	let data = message.data;
-	let length = (data.length/2) - 2;
-	let lengthFull = data.length - 1;	// Do not count opposite store.
-	// Go from right.
-	for(let i = length; 0 <= i; i--){
-		let stepsToStore = length - i + 1;
-		// Look for first match to store from right.
-		if(data[i]%lengthFull === stepsToStore){
-			message.respond(i);
-			return;
+onmessage = init => {
+	onmessage = message => {
+		let data = message.data;
+		let length = (data.length/2) - 2;
+		let lengthFull = data.length - 1;	// Do not count opposite store.
+		// Go from right.
+		for(let i = length; 0 <= i; i--){
+			let stepsToStore = length - i + 1;
+			// Look for first match to store from right.
+			if(data[i]%lengthFull === stepsToStore){
+				postMessage(i);
+				return;
+			}
 		}
-	}
-	// Select first from right with pallets.
-	for(let i = length; 0 <= i; i--){
-		if(0 < data[i]){
-			message.respond(i);
-			return;
+		// Select first from right with pallets.
+		for(let i = length; 0 <= i; i--){
+			if(0 < data[i]){
+				postMessage(i);
+				return;
+			}
 		}
 	}
 }
